44496d6264391b93af3d3bb0e6f28ded43a1e3c4
s6learia <s6learia@uni-bonn.de>
2023-02-28 21:49:51 +0100

    Started working on the data-managment.

--- src/final_project/data_management/clean_data.py
+++ src/final_project/data_management/clean_data.py
@@ -1,36 +1,60 @@
 """Function(s) for cleaning the data set(s)."""
 
-import pandas as pd
 
+def integer_name(df, names, newnames=None):
+    """Convert variable from floats to integers and rename it if wishes.
 
-def clean_data(data, data_info):
-    """Clean data set.
-
-    Information on data columns is stored in ``data_management/data_info.yaml``.
+    Needed as in STATA most variables are floats.
+    Important for id variables that need to be very precise.
 
     Args:
-        data (pandas.DataFrame): The data set.
-        data_info (dict): Information on data set stored in data_info.yaml. The
-            following keys can be accessed:
-            - 'outcome': Name of dependent variable column in data
-            - 'outcome_numerical': Name to be given to the numerical version of outcome
-            - 'columns_to_drop': Names of columns that are dropped in data cleaning step
-            - 'categorical_columns': Names of columns that are converted to categorical
-            - 'column_rename_mapping': Old and new names of columns to be renamend,
-                stored in a dictionary with design: {'old_name': 'new_name'}
-            - 'url': URL to data set
+        df: The data set.
+        names: Name of id variable that needs to be converted.
+        newnames: if the one wants to change the name of the variable.
+
+    Returns:
+        pandas.DataFrame: The data set with converted id variables.
+
+    """
+    if newnames is None:
+        df[names] = df[names].astype(int)
+        return df
+    else:
+        df[newnames] = df[names].astype(int)
+        df = df.drop(columns=names)
+        return df
+
+
+def clean_data(df):
+    """Clean data set.
+
+    Args:
+        df: data set with all appended years.
 
     Returns:
         pandas.DataFrame: The cleaned data set.
 
     """
-    data = data.drop(columns=data_info["columns_to_drop"])
-    data = data.dropna()
-    for cat_col in data_info["categorical_columns"]:
-        data[cat_col] = data[cat_col].astype("category")
-    data = data.rename(columns=data_info["column_rename_mapping"])
+    df = integer_name(df, ["hhseq", "hhid"])
+    df = df[
+        [
+            "age",
+            "year",
+            "wkslyr",
+            "hrslyr",
+            "female",
+            "wgt",
+            "marstat",
+            "hhrel2",
+            "married",
+            "hhseq",
+            "rhrwage",
+            "hhid",
+        ]
+    ]
 
-    numerical_outcome = pd.Categorical(data[data_info["outcome"]]).codes
-    data[data_info["outcome_numerical"]] = numerical_outcome
+    df = df[(df.age > 24) | (df.age < 55)]
 
-    return data
+    df["anual_worked_hours"] = df["wkslyr"] * df["hrslyr"]
+
+    return df
--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -4,21 +4,45 @@ import pandas as pd
 import pytask
 
 from final_project.config import BLD, SRC
-from final_project.data_management import clean_data
-from final_project.utilities import read_yaml
+from final_project.data_management.clean_data import clean_data
 
 
 @pytask.mark.depends_on(
     {
-        "scripts": ["clean_data.py"],
-        "data_info": SRC / "data_management" / "data_info.yaml",
-        "data": SRC / "data" / "data.csv",
+        "1980": SRC / "data" / "cepr_march_1980.dta",
+        "1985": SRC / "data" / "cepr_march_1985.dta",
+        "1990": SRC / "data" / "cepr_march_1990.dta",
+        "1995": SRC / "data" / "cepr_march_1995.dta",
+        "2000": SRC / "data" / "cepr_march_2000.dta",
+        "2005": SRC / "data" / "cepr_march_2005.dta",
+        "2010": SRC / "data" / "cepr_march_2010.dta",
+        "2015": SRC / "data" / "cepr_march_2015.dta",
     },
 )
 @pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
-def task_clean_data_python(depends_on, produces):
-    """Clean the data (Python version)."""
-    data_info = read_yaml(depends_on["data_info"])
-    data = pd.read_csv(depends_on["data"])
-    data = clean_data(data, data_info)
+def task_clean_data(depends_on, produces):
+    """task."""
+    data_1980 = pd.read_stata(depends_on["1980"], convert_categoricals=False)
+    data_1985 = pd.read_stata(depends_on["1985"], convert_categoricals=False)
+    data_1990 = pd.read_stata(depends_on["1990"], convert_categoricals=False)
+    data_1995 = pd.read_stata(depends_on["1995"], convert_categoricals=False)
+    data_2000 = pd.read_stata(depends_on["2000"], convert_categoricals=False)
+    data_2005 = pd.read_stata(depends_on["2005"], convert_categoricals=False)
+    data_2010 = pd.read_stata(depends_on["2010"], convert_categoricals=False)
+    data_2015 = pd.read_stata(depends_on["2015"], convert_categoricals=False)
+
+    res = [
+        data_1980,
+        data_1985,
+        data_1990,
+        data_1995,
+        data_2000,
+        data_2005,
+        data_2010,
+        data_2015,
+    ]
+
+    total = pd.concat(res)
+
+    data = clean_data(total)
     data.to_csv(produces, index=False)
