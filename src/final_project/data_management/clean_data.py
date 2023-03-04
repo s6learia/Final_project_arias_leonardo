357e0c57c99f059c9fca6634bdc07e298abe7792
s6learia <s6learia@uni-bonn.de>
2023-03-04 15:02:56 +0100

    Finished the data management.

--- src/final_project/data_management/clean_data.py
+++ src/final_project/data_management/clean_data.py
@@ -1,42 +1,23 @@
 """Function(s) for cleaning the data set(s)."""
+import numpy as np
+import pandas as pd
 
 
-def integer_name(df, names, newnames=None):
-    """Convert variable from floats to integers and rename it if wishes.
+def clean_data(data):
+    """Clean the data set by dropping all variables that are not going to be use and
+    only keeping individuals in prime age (25 to 56 years old). Besides that, the
+    variable annual working hours is created.
 
-    Needed as in STATA most variables are floats.
-    Important for id variables that need to be very precise.
-
+    
     Args:
-        df: The data set.
-        names: Name of id variable that needs to be converted.
-        newnames: if the one wants to change the name of the variable.
-
-    Returns:
-        pandas.DataFrame: The data set with converted id variables.
-
-    """
-    if newnames is None:
-        df[names] = df[names].astype(int)
-        return df
-    else:
-        df[newnames] = df[names].astype(int)
-        df = df.drop(columns=names)
-        return df
-
-
-def clean_data(df):
-    """Clean data set.
-
-    Args:
-        df: data set with all appended years.
+        data: data set with all appended years.
 
     Returns:
         pandas.DataFrame: The cleaned data set.
 
     """
-    df = integer_name(df, ["hhseq", "hhid"])
-    df = df[
+    data["hhseq"] = data["hhseq"].astype(int)
+    data = data[
         [
             "age",
             "year",
@@ -49,12 +30,40 @@ def clean_data(df):
             "married",
             "hhseq",
             "rhrwage",
-            "hhid",
         ]
     ]
 
-    df = df[(df.age > 24) | (df.age < 55)]
+    data = data[(data.age > 24) | (data.age < 55)]
 
-    df["anual_worked_hours"] = df["wkslyr"] * df["hrslyr"]
+    data["anual_worked_hours"] = data["wkslyr"] * data["hrslyr"]
 
-    return df
+    return data
+
+
+def data_analysis(data):
+    """Modify the cleaned data set for later use in the analysis part. First, drops all
+    members of the household that are not head and spouse. Second, drops all unmarried
+    couples. Third, keeps only household where both head and spouse were surveyed.
+    Finally, gets the variables that are going to be used in the regression.
+
+    Args:
+        df: cleaned data set as csv-file.
+
+    Returns:
+        pandas.DataFrame: The modified cleaned data set.
+
+    """
+    data = data[(data.hhrel2 == 1) | (data.hhrel2 == 2)]
+    data = data[(data.married == 1)]
+    data_count = data.groupby(["hhseq", "year"])["hhseq"].count()
+    data_count = pd.DataFrame(data_count)
+    data_count = data_count.rename(columns={"hhseq": "hh_count"})
+    merge_data = pd.merge(data, data_count, how="inner", on=["hhseq", "year"])
+    merge_data = merge_data[(merge_data.hh_count == 2)]
+    merge_data = merge_data.sort_values(["year", "hhseq", "female"])
+    merge_data["log_wage"] = np.log(merge_data["rhrwage"])
+    merge_data["log_hours"] = np.log(merge_data["anual_worked_hours"])
+    merge_data["husband_wage"] = merge_data["log_wage"].shift(1)
+    merge_data = merge_data[(merge_data.female == 0)]
+
+    return merge_data
