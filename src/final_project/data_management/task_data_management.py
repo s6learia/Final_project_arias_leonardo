98bcf55ec14ba0a54d2826a068538ab6ec44a7d7
s6learia <s6learia@uni-bonn.de>
2023-03-21 16:58:24 +0100

    Fixed data folder.

--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -1,5 +1,7 @@
 """Tasks for managing the data."""
 
+import zipfile
+
 import pandas as pd
 import pytask
 
@@ -7,29 +9,50 @@ from final_project.config import BLD, SRC
 from final_project.data_management.clean_data import clean_data
 
 
-@pytask.mark.depends_on(
-    {
-        "1980": SRC / "data" / "cepr_march_1980.dta",
-        "1985": SRC / "data" / "cepr_march_1985.dta",
-        "1990": SRC / "data" / "cepr_march_1990.dta",
-        "1995": SRC / "data" / "cepr_march_1995.dta",
-        "2000": SRC / "data" / "cepr_march_2000.dta",
-        "2005": SRC / "data" / "cepr_march_2005.dta",
-        "2010": SRC / "data" / "cepr_march_2010.dta",
-        "2015": SRC / "data" / "cepr_march_2015.dta",
-    },
-)
+@pytask.mark.depends_on(SRC / "data" / "cepr_march.zip")
+@pytask.mark.produces(BLD / "python" / "data")
+def task_unzip(depends_on, produces):
+    """Task for unzipping data."""
+    with zipfile.ZipFile(depends_on, "r") as zip_ref:
+        zip_ref.extractall(produces)
+
+
+@pytask.mark.depends_on(BLD / "python" / "data")
 @pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
 def task_clean_data(depends_on, produces):
-    """task."""
-    data_1980 = pd.read_stata(depends_on["1980"], convert_categoricals=False)
-    data_1985 = pd.read_stata(depends_on["1985"], convert_categoricals=False)
-    data_1990 = pd.read_stata(depends_on["1990"], convert_categoricals=False)
-    data_1995 = pd.read_stata(depends_on["1995"], convert_categoricals=False)
-    data_2000 = pd.read_stata(depends_on["2000"], convert_categoricals=False)
-    data_2005 = pd.read_stata(depends_on["2005"], convert_categoricals=False)
-    data_2010 = pd.read_stata(depends_on["2010"], convert_categoricals=False)
-    data_2015 = pd.read_stata(depends_on["2015"], convert_categoricals=False)
+    """Task for cleaning the data."""
+    data_1980 = pd.read_stata(
+        depends_on / "cepr_march_1980.dta",
+        convert_categoricals=False,
+    )
+    data_1985 = pd.read_stata(
+        depends_on / "cepr_march_1985.dta",
+        convert_categoricals=False,
+    )
+    data_1990 = pd.read_stata(
+        depends_on / "cepr_march_1990.dta",
+        convert_categoricals=False,
+    )
+    data_1995 = pd.read_stata(
+        depends_on / "cepr_march_1995.dta",
+        convert_categoricals=False,
+    )
+    data_2000 = pd.read_stata(
+        depends_on / "cepr_march_2000.dta",
+        convert_categoricals=False,
+    )
+    data_2005 = pd.read_stata(
+        depends_on / "cepr_march_2005.dta",
+        convert_categoricals=False,
+    )
+    data_2010 = pd.read_stata(
+        depends_on / "cepr_march_2010.dta",
+        convert_categoricals=False,
+    )
+    data_2015 = pd.read_stata(
+        depends_on / "cepr_march_2015.dta",
+        convert_categoricals=False,
+    )
 
     res = [
         data_1980,
